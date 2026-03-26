a78320e5abe574f039cb60082fd599b52db98267
Jeremy-borderieux <jeremybor@orange.fr>
2026-03-26 13:29:10 -0700

    Jeremy made progress on root biomass tests, comments coming soon

--- scripts/root_biomass_JB.R
+++ scripts/root_biomass_JB.R
@@ -122,13 +122,49 @@ bayesian_model <- brm( pft_biomass |  trunc(lb=0)   ~  treatment , # |  trunc(lb
 
 summary(bayesian_model,prob = 0.9)
 
-new_data <-  data.table(treatment = unique(shrub$treatment))
+new_data <-  data.table(treatment = sort(unique(shrub$treatment)))
 
 preds <- cbind(new_data,fitted(bayesian_model,newdata = new_data,probs = c(0.05,0.95)))
 
-ggplot(preds,aes(y = treatment,x = Estimate,xmin =  Q5, xmax = Q95))+
-  geom_pointrange()+
-  theme_classic()
+preds_full <- data.table(fitted(bayesian_model,newdata = new_data,summary = F))
+colnames(preds_full) <- as.character(new_data$treatment)
+
+preds_delta <- preds_full[,.(`Heat wave` = `Heat wave` - Control ,
+                             `Extended season` =`Extended season` - Control)]
+
+preds_full <- melt(preds_full,variable.name = "treatment",value.name = "Estimate")
+preds_full$treatment <- as.factor(preds_full$treatment)
+
+preds_delta <- melt(preds_delta,variable.name = "treatment",value.name = "Estimate")
+
+ggplot(preds,aes(x = treatment,y = Estimate))+
+  geom_violin(data = preds_full,aes(fill = treatment))+
+  geom_pointrange(aes(ymin =  Q5, ymax = Q95))+
+  geom_point(data = root_data,aes(y = ,x = treatment))+
+  theme_classic()+  
+  scale_fill_manual(values = c("Control" = "#6c6563",
+                             "Heat wave" = "#b56d5e",
+                              "Extended season" = "#bbbc81")) 
+
+preds_delta[,1-mean(Estimate>0),by = treatment]
+preds_delta[,mean(Estimate),by = treatment]
+
+ggplot(preds_delta,aes(x = Estimate,fill = treatment ))+
+  geom_vline(xintercept = 0,lty = 2)+
+  geom_density(alpha = 0.5)+
+  theme_classic()+  
+  scale_fill_manual(values = c("Control" = "#6c6563",
+                               "Heat wave" = "#b56d5e",
+                               "Extended season" = "#bbbc81"))
+
+
+ggplot(preds_delta,aes(x = treatment, y = Estimate ,fill = treatment ))+
+  geom_hline(yintercept = 0,lty = 2)+
+  geom_violin(alpha = 0.95)+
+  theme_classic()+  
+  scale_fill_manual(values = c("Control" = "#6c6563",
+                               "Heat wave" = "#b56d5e",
+                               "Extended season" = "#bbbc81"))
 
 plot(bayesian_model)
 pp_check(bayesian_model)
